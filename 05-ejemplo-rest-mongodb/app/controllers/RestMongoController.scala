9e47bf357785e6cbf677856d1d3872b675e00dd1
David Vaquero <pepesan@gmail.com>
2020-12-02 12:24:53 +0100

    mongodb api rest working

--- 05-ejemplo-rest-mongodb/app/controllers/RestMongoController.scala
+++ 05-ejemplo-rest-mongodb/app/controllers/RestMongoController.scala
@@ -2,9 +2,11 @@ package controllers
 
 import javax.inject._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.collection.mutable
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import reactivemongo.play.json._
 import reactivemongo.play.json.collection._
 import reactivemongo.bson.{BSONDocument, BSONObjectID}
@@ -24,47 +26,60 @@ class RestMongoController @Inject()(
       posts => Ok(Json.toJson(posts))
     }
   }
-  /*
-  def add = Action.async(parse.json) {
-    import play.api.libs.json.Reads._
-    val transformer: Reads[JsObject] =
-      Reads.jsPickBranch[JsString](__ \ "title") and
-        Reads.jsPickBranch[JsString](__ \ "description")  reduce
-        request.body.transform(transformer).map { result =>
-          collection.flatMap(_.insert.one(result)).map { lastError =>
-            Logger.debug(s"Successfully inserted with LastError: $lastError")
-            Created
-          }
-        }.getOrElse(Future.successful(BadRequest("invalid json")))
-    _.body.validate[Post].map{
-      post => postRepository.create(post).map{
-        post => Ok(Json.toJson(post))
-      }
-    }.getOrElse(Future.successful(BadRequest("Invalid format")))
-  }
 
-def read (id: BSONObjectID) = Action.async {
-  postRepository.read(id).map {
-    post => post.map{
-      post => Ok(Json.toJson(post))
+    def add = Action.async(parse.json) {
+      import play.api.libs.json.Reads._
+      /*
+       * request.body is a JsValue.
+       * There is an implicit Writes that turns this JsValue as a JsObject,
+       * so you can call insert.one() with this JsValue.
+       * (insert.one() takes a JsObject as parameter, or anything that can be
+       * turned into a JsObject using a Writes.)
+       */
+      val transformer: Reads[JsObject] =
+        Reads.jsPickBranch[JsString](__ \ "title") and
+          Reads.jsPickBranch[JsString](__ \ "description") reduce
+
+      _.body.transform(transformer).map { result =>
+        postRepository.createJson(result).map { lastError =>
+          //Logger.debug(s"Successfully inserted with LastError: $lastError")
+          Created
+        }
+      }.getOrElse(Future.successful(BadRequest(Json.toJson("invalid format"))))
     }
-  }.getOrElse(NotFound)
-}
 
-def update (id: BSONObjectID) = Action.async(parse.json) {
-  _.body.validate[Post].map{
-    post => postRepository.udpate(id,post).map{
-      case Some(post) => Ok(Json.toJson(post))
-      case _ => NotFound
-    }
-  }.getOrElse(Future.successful(BadRequest("Invalid format")))
-}
-def delete (id: BSONObjectID) = Action.async{
-  postRepository.destroy(id).map{
-    case Some(post) => Ok(Json.toJson(post))
-    case _ => NotFound
-  }
-}
+ def read (id: BSONObjectID) = Action.async {
+   postRepository.read(id).map{
+    post => Ok(Json.toJson(post))
+   }
+ }
 
- */
+ def update (id: BSONObjectID) = Action.async(parse.json) {
+   import play.api.libs.json.Reads._
+   /*
+    * request.body is a JsValue.
+    * There is an implicit Writes that turns this JsValue as a JsObject,
+    * so you can call insert.one() with this JsValue.
+    * (insert.one() takes a JsObject as parameter, or anything that can be
+    * turned into a JsObject using a Writes.)
+    */
+   val transformer: Reads[JsObject] =
+     Reads.jsPickBranch[JsString](__ \ "title") and
+       Reads.jsPickBranch[JsString](__ \ "description") reduce
+
+   _.body.transform(transformer).map { result =>
+     postRepository.update(id, result).map { lastError =>
+       //Logger.debug(s"Successfully inserted with LastError: $lastError")
+       Ok(Json.toJson(result))
+     }
+   }.getOrElse(Future.successful(BadRequest(Json.toJson("invalid format"))))
+
+ }
+
+ def delete (id: BSONObjectID) = Action.async{
+   postRepository.destroy(id).map{
+     case Some(post) => Ok(Json.toJson(post))
+     case _ => NotFound
+   }
+ }
 }
--- 05-ejemplo-rest-mongodb/app/repositories/PostRepository.scala
+++ 05-ejemplo-rest-mongodb/app/repositories/PostRepository.scala
@@ -5,6 +5,8 @@ import scala.concurrent.{ExecutionContext, Future}
 import play.modules.reactivemongo.ReactiveMongoApi
 import reactivemongo.play.json.collection.JSONCollection
 import models.Post
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import reactivemongo.play.json._
 import reactivemongo.bson.{BSONDocument, BSONObjectID}
 import reactivemongo.api.{ReadPreference, Cursor}
@@ -26,17 +28,17 @@ class PostRepository @Inject()(
   def create (post: Post): Future[WriteResult] ={
     collection.flatMap(_.insert(post))
   }
+  def createJson (post: JsObject) ={
+    collection.flatMap(_.insert.one(post))
+  }
   def read (id: BSONObjectID): Future[Option[Post]] = {
     collection.flatMap(_.find(BSONDocument("_id" -> id)).one[Post])
   }
-  def update(id: BSONObjectID, post: Post): Future[Option[Post]] = {
+  def update(id: BSONObjectID, post:JsObject): Future[Option[Post]] = {
     collection.flatMap(_.findAndUpdate(
         BSONDocument("_id" -> id),
         BSONDocument(
-          f"$$set" -> BSONDocument(
-            "title" -> post.title,
-            "description" -> post.description
-          )
+          f"$$set" -> post
         ),
         true
       ).map(_.result[Post])
